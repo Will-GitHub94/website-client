5be6929846a97c12381d92ef763827cc8956aa39
Will Ashworth <wmashworth@hotmail.com>
2018-05-17 18:49:11 +0100

    Added check against hot reloading

--- src/main.js
+++ src/main.js
@@ -15,16 +15,30 @@ Vue.use(Router);
 Vue.config.productionTip = false;
 
 (async () => {
-	await store.dispatch("getReadme");
-	await store.dispatch("getArchitecture");
-	await store.dispatch("getNetworking");
-	await store.dispatch("getCryptography");
-	await store.dispatch("getMachineLearning");
+	let architecturePaths = store.getters.getArchitecturePaths;
+	let networkingPaths = store.getters.getNetworkingPaths;
+	let cryptographyPaths = store.getters.getCryptographyPaths;
+	let machineLearningPaths = store.getters.getMachineLearningPaths;
 
-	const architecturePaths = store.getters.getArchitecturePaths;
-	const networkingPaths = store.getters.getNetworkingPaths;
-	const cryptographyPaths = store.getters.getCryptographyPaths;
-	const machineLearningPaths = store.getters.getMachineLearningPaths;
+	const pathsAlreadyLoaded = architecturePaths.length > 0
+							&& networkingPaths.length > 0
+							&& cryptographyPaths.length > 0
+							&& machineLearningPaths.length > 0;
+
+	// if paths are already populated then don't get them again
+	if (!pathsAlreadyLoaded) {
+		await store.dispatch("getReadme");
+		await store.dispatch("getArchitecture");
+		await store.dispatch("getNetworking");
+		await store.dispatch("getCryptography");
+		await store.dispatch("getMachineLearning");
+
+		architecturePaths = store.getters.getArchitecturePaths;
+		networkingPaths = store.getters.getNetworkingPaths;
+		cryptographyPaths = store.getters.getCryptographyPaths;
+		machineLearningPaths = store.getters.getMachineLearningPaths;
+	}
+	
 
 	const knowledgePaths = {
 		architecturePaths,
